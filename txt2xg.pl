02d066d5a82ced4c55cf720687cddf71df66d100
Geoff McLane <ubuntu@geoffair.info>
2015-08-21 15:30:39 +0200

    Begin to add SID to text file

--- txt2xg.pl
+++ txt2xg.pl
@@ -7,6 +7,7 @@ use strict;
 use warnings;
 use File::Basename;  # split path ($name,$dir,$ext) = fileparse($file [, qr/\.[^.]*/] )
 use Cwd;
+use Math::Trig;
 my $cwd = cwd();
 my $os = $^O;
 my ($pgmname,$perl_dir) = fileparse($0);
@@ -35,14 +36,23 @@ my $out_file = $temp_dir.$PATH_SEP."temptxt2xg.xg";
 my $star_color = 'green';
 my $sid_color  = 'blue';
 my $app_color  = 'white';
+my $usr_anno = '';
+
+my $m_max_path = 300000;    # was 100000
+my $m_path_widthm = 500; # was 300; # was 5000;   
+my $m_arrow_angle = 30;
+my $add_second_end = 1;
+my $add_arrow_sides = 1;
 
 # ### DEBUG ###
 my $debug_on = 1;
 #my $def_file = 'C:\Users\user\Documents\FG\LFPO.procedures.txt';
 my $def_file = $perl_dir.'circuits'.$PATH_SEP.'LFPO.procedures.txt';
+my $def_anno = "anno 2.308119 48.75584 Rue Pernoud, Antony";
 
 ### program variables
 my @warnings = ();
+my $tmp_xg = $temp_dir.$PATH_SEP."temptemp.xg";
 
 sub VERB1() { return $verbosity >= 1; }
 sub VERB2() { return $verbosity >= 2; }
@@ -138,6 +148,7 @@ sub load_fix_hash($) {
         $len = length($line);
         next if ($len == 0);
         next if ($line =~ /^I/);
+        next if ($line =~ /^\d+\s+Version/);
         @arr = split(/\s+/,$line);
         $cnt = scalar @arr;
         $typ = $arr[0];
@@ -152,7 +163,8 @@ sub load_fix_hash($) {
     }
     $rfixhash = \%h;
     $done_fix_hash = 1;
-    @arr = keys %h;
+    ### @arr = keys %h;
+    @arr = keys %{$rfixhash};
     $len = scalar @arr;
     prt("Loaded $len fixes from $fix_file\n");
     return $rfixhash;
@@ -182,6 +194,36 @@ sub search_fix_file($) {
     }
     return $cnt;
 }
+
+sub find_fix_id($$$$) {
+    my ($rfa,$id,$rlat,$rlon) = @_;
+	my $cnt = scalar @{$rfa};
+	my ($i,$ra,$nid,$nlat,$nlon,$line,$len,@arr,$val);
+    my @fnd = ();
+	for ($i = 0; $i < $cnt; $i++) {
+        $line = trim_all(${$rfa}[$i]);
+        $len = length($line);
+        next if ($len == 0);
+        @arr = split(/\s+/,$line);
+        $len = scalar @arr;
+        $val = $arr[0];
+        next if ($val eq 'I');
+        next if ($val == 600);
+        last if ($val == 99);
+        if ($len == 3) {
+    		$nid = $arr[2];
+            if ($nid eq $id) {
+                $nlat = $arr[0];
+                $nlon = $arr[1];
+                ${$rlat} = $nlat;
+                ${$rlon} = $nlon;
+                return 1;
+            }
+        }
+	}
+    return 0;
+}
+
 
 #######################################################################
 # =============================
@@ -313,11 +355,124 @@ sub find_nav_id($$$$) {
         $nlon = ${$ra}[2];
         ${$rlat} = $nlat;
         ${$rlon} = $nlon;
+        prt("Found $id $nlat,$nlon\n");
     }
 }
 
 #######################################################################
 
+# some utility functions
+sub is_all_numeric($) {
+    my ($txt) = shift;
+    $txt = substr($txt,1) if ($txt =~ /^-/);
+    return 1 if ($txt =~ /^(\d|\.)+$/);
+    return 0;
+}
+
+my $show_set_dec_error = 1;
+sub set_decimal1_stg($) {
+    my $r = shift;
+    if (is_all_numeric(${$r})) {
+        ${$r} =  int((${$r} + 0.05) * 10) / 10;
+        ${$r} = "0.0" if (${$r} == 0);
+        ${$r} .= ".0" if !(${$r} =~ /\./);
+    } else {
+        prtw("WARNING: set_decimal1_stg() passed non-numeric value [".${$r}."]") if ($show_set_dec_error);
+    }
+}
+
+sub get_dist_stg_km($) {
+    my ($dist) = @_;
+    my $km = $dist / 1000;
+    set_decimal1_stg(\$km);
+    $km .= "Km";
+    return $km;
+}
+
+#######################################################################
+sub get_arrow_xg($$$$$$$$$) {
+    my ($from,$wlat1,$wlon1,$to,$wlat2,$wlon2,$whdg1,$hwid,$color) = @_;
+    my $wsize = ($hwid / sin(deg2rad($m_arrow_angle)));
+    my ($wlatv1,$wlonv1,$whdg,$wlatv2,$wlonv2,$waz1,$tmp,$tmp2);
+    my ($wlatv3,$wlonv3,$wlatv4,$wlonv4);
+
+    my $xg = '';
+	$tmp = int($whdg1 + 0.5);
+	$tmp2 = int($wsize / 1000);
+	### $xg .= "# end arrows from $wlat1,$wlon1 to $wlat2,$wlon2, hdg $tmp, size $tmp2 km\n";
+	$xg .= "# end arrows from $from to $to, hdg $tmp, size $tmp2 km\n";
+
+    ## $xg .= "color gray\n";
+    $xg .= "color $color\n";
+    $whdg = $whdg1 + $m_arrow_angle; # was 30
+    $whdg -= 360 if ($whdg > 360);
+    fg_geo_direct_wgs_84($wlat1,$wlon1, $whdg, $wsize, \$wlatv1, \$wlonv1, \$waz1 );
+    fg_geo_direct_wgs_84($wlat2,$wlon2, $whdg, $wsize, \$wlatv2, \$wlonv2, \$waz1 );
+
+    $xg .= "$wlon1 $wlat1\n";
+    $xg .= "$wlonv1 $wlatv1\n";
+    $xg .= "NEXT\n";
+
+    if ($add_second_end) {
+        $xg .= "$wlon2 $wlat2\n";
+        $xg .= "$wlonv2 $wlatv2\n";
+        $xg .= "NEXT\n";
+    }
+
+    if ($add_arrow_sides) {
+        $xg .= "$wlonv1 $wlatv1\n";
+        $xg .= "$wlonv2 $wlatv2\n";
+        $xg .= "NEXT\n";
+    }
+
+    $whdg = $whdg1 - $m_arrow_angle; # was 30
+    $whdg += 360 if ($whdg < 0);
+    fg_geo_direct_wgs_84($wlat1,$wlon1, $whdg, $wsize, \$wlatv3, \$wlonv3, \$waz1 );
+    fg_geo_direct_wgs_84($wlat2,$wlon2, $whdg, $wsize, \$wlatv4, \$wlonv4, \$waz1 );
+
+    $xg .= "$wlon1 $wlat1\n";
+    $xg .= "$wlonv3 $wlatv3\n";
+    $xg .= "NEXT\n";
+
+    if ($add_second_end) {
+        $xg .= "$wlon2 $wlat2\n";
+        $xg .= "$wlonv4 $wlatv4\n";
+        $xg .= "NEXT\n";
+    }
+
+    if ($add_arrow_sides) {
+        $xg .= "$wlonv3 $wlatv3\n";
+        $xg .= "$wlonv4 $wlatv4\n";
+        $xg .= "NEXT\n";
+    }
+    ### prt($xg);
+    return $xg;
+}
+
+sub get_path_xg($$$$$$$) {
+    my ($from,$elat1,$elon1,$to,$elat2,$elon2,$color) = @_;
+    my ($az1,$az2,$s,$res);
+    my $hwidm = $m_path_widthm; # 300; # was 5000;
+    my $xg = '';
+    $res = fg_geo_inverse_wgs_84($elat1,$elon1,$elat2,$elon2,\$az1,\$az2,\$s);
+    if ($s < $hwidm) {
+        $s = (int($s * 10) / 10);
+        return "# Dist too small $from $elat1,$elon1 $to $elat2,$elon2 $s, min $hwidm\n";
+    } elsif ($s > $m_max_path ) { # was 100000
+        $s = (int($s * 10) / 10);
+        $res = $m_max_path / 1000;
+        return "# Dist too large $from $elat1,$elon1 $to $elat2,$elon2 $s, GT $res Km\n";
+    }
+    my $wkm = get_dist_stg_km($hwidm * 2);
+    my $lkm = get_dist_stg_km($s);
+    $xg .= "# Rect using $elat1,$elon1 $elat2,$elon2 len $lkm, wid $wkm\n";
+    # NOTE: Was developed using Wind, so NOTE the reverse track used
+    # $xg .= get_arrow_xg($from,$elat1,$elon1,$to,$elat2,$elon2,$az1,$hwidm,$color); # 'orange'
+    $xg .= get_arrow_xg($from,$elat1,$elon1,$to,$elat2,$elon2,$az2,$hwidm,$color); # 'orange'
+    return $xg;
+}
+
+#######################################################################
 sub process_in_file($) {
     my ($inf) = @_;
     if (! open INF, "<$inf") {
@@ -329,7 +484,7 @@ sub process_in_file($) {
     prt("Processing $lncnt lines, from [$inf]...\n");
     my ($line,$inc,$lnn,$len,$section,@arr,$cnt,$i);
     my ($ns,$ew,$lat,$lon,$dlat,$dlon,$name);
-    my (@arr2,$wp,$ra,$color,$plat,$plon,$pcnt);
+    my (@arr2,$wp,$ra,$color,$plat,$plon,$pcnt,$pwp);
     $lnn = 0;
     my %waypoints = ();
     #my $rfh = search_fix_file("MATIX");
@@ -337,7 +492,14 @@ sub process_in_file($) {
     my $rfh = load_fix_hash($rfa);
 	my $rna = load_nav_file();
 
+    my $max_lat = -400;
+    my $max_lon = -400;
+    my $min_lat = 400;
+    my $min_lon = 400;
+    my ($clat,$clon);
     $section = '';
+    my $wpxg = "# Waypoints found in text\n";
+    $pcnt = 0;
     foreach $line (@lines) {
         chomp $line;
         $line = trim_all($line);
@@ -380,6 +542,14 @@ sub process_in_file($) {
                     $lon += $dlon / 60;
                     prt("$lnn: $name $lat,$lon\n") if (VERB9());
                     $waypoints{$name} = [$lat,$lon];
+                    $max_lat = $lat if ($lat > $max_lat);
+                    $max_lon = $lon if ($lon > $max_lon);
+                    $min_lat = $lat if ($lat < $min_lat);
+                    $min_lon = $lon if ($lon < $min_lon);
+                    $wpxg .= "anno $lon $lat $name\n";
+                    $wpxg .= "$lon $lat\n";
+                    $wpxg .= "NEXT\n";
+                    $pcnt++;
                 } else {
                     prtw("WARNING:$lnn: [$inc] failed regex\n");
                 }
@@ -388,22 +558,59 @@ sub process_in_file($) {
             }
         }
     }
+
+    write2file($wpxg,$tmp_xg);
+    prt("Written waypoints collected to $tmp_xg\n");
+
+    $clat = ($max_lat + $min_lat) / 2;
+    $clon = ($max_lon + $min_lon) / 2;
     @arr = keys %{$rfh};
+    $len = 0;
     foreach $name (@arr) {
-        $waypoints{$name} = ${$rfh}{$name}; # [$lat,$lon];
+        $ra = ${$rfh}{$name};
+        $lat = ${$ra}[0];
+        $lon = ${$ra}[1];
+        if (defined $waypoints{$name}) {
+            # how to choose
+        } else {
+            $waypoints{$name} = [$lat,$lon]; # ${$rfh}{$name}; # 
+            $len++;
+        }
     }
+    prt("Added $len fixes to waypoints hash...\n");
+    $len = 0;
+    my ($res,$az1,$az2,$s,$s2);
     foreach $ra (@{$rna}) {
         $lat = ${$ra}[1];
         $lon = ${$ra}[2];
 		$name  = ${$ra}[6];
-        $waypoints{$name} = [$lat,$lon];
+        # UGH! This id could be a duplicate
+        if (defined $waypoints{$name}) {
+            $res = fg_geo_inverse_wgs_84($clat,$clon,$lat,$lon,\$az1,\$az2,\$s);
+            if ($s < 300000) {
+                $wp = $name;
+                $ra = $waypoints{$wp};
+                $plat = ${$ra}[0];
+                $plon = ${$ra}[1];
+                $res = fg_geo_inverse_wgs_84($clat,$clon,$plat,$plon,\$az1,\$az2,\$s2);
+                if ($s < $s2) {
+                    $waypoints{$name} = [$lat,$lon];
+                    $len++;
+                }
+            }
+        } else {
+            $waypoints{$name} = [$lat,$lon];
+            $len++;
+        }
 	}
+    prt("Added $len navaids to waypoints hash...\n");
 
     $lnn = 0;
     $section = '';
     my %dupes = ();
     my %setwp = ();
     my $xg = "# sid/star/app from $inf\n";
+    my ($msg);
     foreach $line (@lines) {
         chomp $line;
         $line = trim_all($line);
@@ -439,15 +646,19 @@ sub process_in_file($) {
                     $inc = trim_all($arr2[$i]);
                     $arr2[$i] = $inc;
                 }
-                prt("$lnn:star $name = ".join(" ",@arr2)."\n");
+                $msg = "$lnn:star $name = ".join(" ",@arr2)." ($cnt)";
+                prt("$msg\n") if (VERB5());
+                $xg .= "# $msg\n";
                 $pcnt = 0;
                 for ($i = 0; $i < $cnt; $i++) {
                     $wp = $arr2[$i];
                     last if ($wp =~ /^\#/);
                     if (defined $waypoints{$wp}) {
-                        $ra = $waypoints{$name};
-                        $lat = ${$ra}[0];
-                        $lon = ${$ra}[1];
+                        #if (!find_fix_id($rfa,$wp,\$lat,\$lon)) {
+                            $ra = $waypoints{$wp};
+                            $lat = ${$ra}[0];
+                            $lon = ${$ra}[1];
+                        #}
                         $xg .= "color $color\n";
                         if (!defined $setwp{$wp}) {
                             $setwp{$wp} = 1;
@@ -456,8 +667,12 @@ sub process_in_file($) {
                         $xg .= "$lon $lat\n";
                         $xg .= "NEXT\n";
                         if ($pcnt) {
+                            $xg .= get_path_xg($pwp,$plat,$plon,$wp,$lat,$lon,$color);
 
                         }
+                        $plat = $lat;
+                        $plon = $lon;
+                        $pwp  = $wp;
                         $pcnt++;
                     } else {
                         if (! defined $dupes{$wp}) {
@@ -470,6 +685,53 @@ sub process_in_file($) {
             } elsif ($section =~ /^SID/) {
                 # to do
                 $color = $sid_color;
+                @arr2 = split(/\s+/,$name);
+                $wp = $arr2[0]; # destination of this departure
+
+                @arr2 = split("-",$inc);
+                $cnt = scalar @arr2;
+                for ($i = 0; $i < $cnt; $i++) {
+                    $inc = trim_all($arr2[$i]);
+                    $arr2[$i] = $inc;
+                }
+                prt("$lnn:sid $name = ".join(" ",@arr2)."\n");
+                $pcnt = 0;
+                if (defined $waypoints{$wp}) {
+                    for ($i = 0; $i < $cnt; $i++) {
+                        $wp = $arr2[$i];
+                        last if ($wp =~ /^\#/);
+                        if (defined $waypoints{$wp}) {
+                            $ra = $waypoints{$wp};
+                            $lat = ${$ra}[0];
+                            $lon = ${$ra}[1];
+                            $xg .= "color $color\n";
+                            if (!defined $setwp{$wp}) {
+                                $setwp{$wp} = 1;
+                                $xg .= "anno $lon $lat $wp\n";
+                            }
+                            $xg .= "$lon $lat\n";
+                            $xg .= "NEXT\n";
+                            if ($pcnt) {
+                                $xg .= get_path_xg($pwp,$plat,$plon,$wp,$lat,$lon,$color);
+
+                            }
+                            $plat = $lat;
+                            $plon = $lon;
+                            $pwp  = $wp;
+                            $pcnt++;
+                        } else {
+                            if (! defined $dupes{$wp}) {
+                                $dupes{$wp} = 1;
+                                prtw("WARNING: app waypoint [$wp] NOT in hash!\n");
+                            }
+                        }
+                    }
+                } else {
+                    if (! defined $dupes{$wp}) {
+                        $dupes{$wp} = 1;
+                        prtw("WARNING: app waypoint [$wp] NOT in hash!\n");
+                    }
+                }
             } elsif ($section =~ /^APP/) {
                 $color = $app_color;
                 @arr2 = split("-",$inc);
@@ -478,12 +740,30 @@ sub process_in_file($) {
                     $inc = trim_all($arr2[$i]);
                     $arr2[$i] = $inc;
                 }
-                prt("$lnn:app $name = ".join(" ",@arr2)."\n");
+                prt("$lnn:app $name = ".join(" ",@arr2)."\n") if (VERB5());
+                $wp = $name;
+                $pcnt = 0;
+                if (defined $waypoints{$wp}) {
+                    $ra = $waypoints{$wp};
+                    $lat = ${$ra}[0];
+                    $lon = ${$ra}[1];
+                    $xg .= "color $color\n";
+                    if (!defined $setwp{$wp}) {
+                        $setwp{$wp} = 1;
+                        $xg .= "anno $lon $lat $wp\n";
+                    }
+                    $xg .= "$lon $lat\n";
+                    $xg .= "NEXT\n";
+                    $plat = $lat;
+                    $plon = $lon;
+                    $pwp  = $wp;
+                    $pcnt++;
+                }
                 for ($i = 0; $i < $cnt; $i++) {
                     $wp = $arr2[$i];
                     last if ($wp =~ /^\#/);
                     if (defined $waypoints{$wp}) {
-                        $ra = $waypoints{$name};
+                        $ra = $waypoints{$wp};
                         $lat = ${$ra}[0];
                         $lon = ${$ra}[1];
                         $xg .= "color $color\n";
@@ -493,6 +773,14 @@ sub process_in_file($) {
                         }
                         $xg .= "$lon $lat\n";
                         $xg .= "NEXT\n";
+                        if ($pcnt) {
+                            $xg .= get_path_xg($pwp,$plat,$plon,$wp,$lat,$lon,$color);
+
+                        }
+                        $plat = $lat;
+                        $plon = $lon;
+                        $pwp  = $wp;
+                        $pcnt++;
                     } else {
                         if (! defined $dupes{$wp}) {
                             $dupes{$wp} = 1;
@@ -506,6 +794,17 @@ sub process_in_file($) {
                 pgm_exit(1,"Error: Section [$section] not coded! *** FIX ME ***\n");
             }
         }
+    }
+
+    if (length($usr_anno)) {
+        $xg .= "$usr_anno\n";
+        @arr = split(/\s+/,$usr_anno);
+        $lon = $arr[1];
+        $lat = $arr[2];
+        $xg .= "color red\n";
+        $xg .= "$lon $lat\n";
+        # $xg .= "$clon $clat\n"; # no this is just the center of the wapoints collected
+        $xg .= "NEXT\n";
     }
 
     rename_2_old_bak($out_file);
@@ -594,6 +893,9 @@ sub parse_args {
         if (length($in_file) ==  0) {
             $in_file = $def_file;
             prt("Set DEFAULT input to [$in_file]\n");
+        }
+        if (length($usr_anno) == 0) {
+            $usr_anno = $def_anno;
         }
     }
     if (length($in_file) ==  0) {
